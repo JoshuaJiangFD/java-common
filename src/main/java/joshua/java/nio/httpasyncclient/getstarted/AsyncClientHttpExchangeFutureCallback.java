4024fd8afb73826368936953fa8017931939842c
Joshua Jiang <Joshua.Jiang@outlook.com>
2016-03-14 23:14:14 +0800

    checkin a few examples for httpasyncclient

---
+++ src/main/java/joshua/java/nio/httpasyncclient/getstarted/AsyncClientHttpExchangeFutureCallback.java
@@ -0,0 +1,64 @@
+package joshua.java.nio.httpasyncclient.getstarted;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+
+/**
+ * This example demonstrates a fully asynchronous execution of multiple HTTP exchanges
+ * where the result of an individual operation is reported using a callback interface.
+ */
+public class AsyncClientHttpExchangeFutureCallback {
+
+    public static void main(final String[] args) throws Exception {
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setSocketTimeout(3000)
+                .setConnectTimeout(3000).build();
+        CloseableHttpAsyncClient httpclient = HttpAsyncClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        try {
+            httpclient.start();
+            final HttpGet[] requests = new HttpGet[]{
+                    new HttpGet("http://www.apache.org/"),
+                    new HttpGet("https://www.verisign.com/"),
+                    new HttpGet("http://www.google.com/")
+            };
+            final CountDownLatch latch = new CountDownLatch(requests.length);
+            for (final HttpGet request : requests) {
+                httpclient.execute(request, new FutureCallback<HttpResponse>() {
+
+                    @Override
+                    public void completed(final HttpResponse response) {
+                        latch.countDown();
+                        System.out.println(request.getRequestLine() + "->" + response.getStatusLine());
+                    }
+
+                    @Override
+                    public void failed(final Exception ex) {
+                        latch.countDown();
+                        System.out.println(request.getRequestLine() + "->" + ex);
+                    }
+
+                    @Override
+                    public void cancelled() {
+                        latch.countDown();
+                        System.out.println(request.getRequestLine() + " cancelled");
+                    }
+
+                });
+            }
+            latch.await();
+            System.out.println("Shutting down");
+        } finally {
+            httpclient.close();
+        }
+        System.out.println("Done");
+    }
+
+}
---
+++ src/main/java/joshua/java/nio/httpasyncclient/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package includes all the examples, trials and demos
+ * regarding to usage of <b>HttpAsyncClient</b> library.<br>
+ * This library is based on non-blocking IO, which is fundamentally important to HA web applications.<br>
+ *
+ * @see <a href="http://hc.apache.org/httpcomponents-asyncclient-dev/index.html">HttpAsyncClient site page</a>
+ *
+ *
+ * @author Joshua.Jiang on 2016/3/14.
+ */
+package joshua.java.nio.httpasyncclient;
